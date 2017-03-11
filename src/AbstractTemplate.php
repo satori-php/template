ddf3b1371203d60ddd0c87ba3571c2c0d597d46d
Yuriy Davletshin <yuriy.davletshin@gmail.com>
2017-03-11 16:01:14 +0200

    Fixed file-level docblock

--- src/AbstractTemplate.php
+++ src/AbstractTemplate.php
@@ -1,11 +1,13 @@
 <?php
-declare(strict_types=1);
 
 /**
  * @author    Yuriy Davletshin <yuriy.davletshin@gmail.com>
  * @copyright 2017 Yuriy Davletshin
  * @license   MIT
  */
+
+declare(strict_types=1);
+
 namespace Satori\Template;
 
 /**
